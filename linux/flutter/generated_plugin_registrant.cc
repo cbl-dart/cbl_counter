3f3cc0028178d5e291306fe2756402aa0df80d6f
Gabriel Terwesten <gabriel@terwesten.net>
2024-11-27 09:34:07 +0100

    Upgrade Linux project

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <cbl_flutter_ce/cbl_flutter_ce.h>
+#include <cbl_flutter_ee/cbl_flutter_ee.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) cbl_flutter_ce_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "CblFlutterCe");
-  cbl_flutter_ce_register_with_registrar(cbl_flutter_ce_registrar);
+  g_autoptr(FlPluginRegistrar) cbl_flutter_ee_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "CblFlutterEe");
+  cbl_flutter_ee_register_with_registrar(cbl_flutter_ee_registrar);
 }
